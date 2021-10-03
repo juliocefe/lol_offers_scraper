09b0dfbb06eeeb8ed7c2cbcad75b2122a425076e
juliocefe <juliocesarfelixhz95@gmail.com>
2021-10-02 19:37:10 -0700

    firs commit

---
+++ gspread_library.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+from operator import itemgetter
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials_path = 'secret/credentials.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, scope)
+gc = gspread.authorize(credentials)
+
+def get_data_from_gsheet(url):
+    workbook = gc.open_by_url(url)
+    skins = workbook.sheet1.get_all_records()
+    return skins
---
+++ mail.py
@@ -0,0 +1,29 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('MAIL')
+EMAIL_PASSWORD = os.environ.get('PASS')
+EMAIL_CONTACTS = os.environ.get('CONTACTS').split(" ")
+
+contacts = EMAIL_CONTACTS
+def send_email(found_skins):
+    msg = EmailMessage()
+    msg['Subject'] = 'SKINS!!'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = contacts
+    content = f'Parece que encontramos unas skins en un oferta Julio :3'
+    for skin in found_skins:
+        found = f"\n  - {skin['champion']}, con la skin {skin['skin']}"
+        content = content + found
+    msg.set_content(content)
+    with open('screenshot.png','rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+        
+    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
